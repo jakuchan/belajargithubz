342461621e1595a7a451cab7da55c1b8971610f2
jakuchan <114452214+jakuchan@users.noreply.github.com>
2022-09-27 08:55:06 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title>Selamat Datang</title>
+  <link rel="stylesheet" type="text/css" href="navi.css">
+  <link rel="stylesheet" type="text/css" href="php.css">
+  <link rel="stylesheet" href="form.css">
+
+
+</head>
+
+<body>
+
+
+  <div class="menu">
+    <ul id="navigasi">
+      
+      <li><a href="index.php#">Home</a></li>
+      <li><a href="form.php">Form</a></li>
+      <li><a href="tabel.php">Tabel</a>
+      <li><a href="conif.php">SPP</a></li>
+      <li><a id="logo" href="radiobut.php">Logout</a>
+      </li>
+    </ul>
+  </div>
+  <h1>Selamat Datang</h1>
+                    
+</body>
+
+</html>
